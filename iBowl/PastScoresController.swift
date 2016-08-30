1ec470c3a197d89d65455593bf20d8a7d0530767
lucasmarzocco <lucasmarzocco@gmail.com>
2016-08-30 13:12:54 -0700

    figuring out retrieval of info

--- iBowl/PastScoresController.swift
+++ iBowl/PastScoresController.swift
@@ -13,13 +13,10 @@ class PastScoresController: UITableViewController {
     
     var ref = FIRDatabase.database().referenceFromURL("https://ibowl-c7e9e.firebaseio.com")
     
-    
     //var scores: [String] = ["6/7/2016", "6/10/2016", "6/14/2016", "6/27/2016"]
     //var wow: [String] = ["Average: 202 - High game: 220", "Average: 206 - High game: 300", "Average: 210 - High game: 200", "Average: 202 - High game: 189" ]
     
-    
     var dates: [String] = []
-    //var keys: [String] = ["highGame", "]
     
     override func viewDidLoad() {
         
@@ -30,12 +27,11 @@ class PastScoresController: UITableViewController {
             for item in postDict.keys {
                 print(item)
                 
-                
-                
-                
-
+                self.dates.append(item)
             }
         })
+        
+        print(self.dates)
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
@@ -44,18 +40,16 @@ class PastScoresController: UITableViewController {
     
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return self.dates.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        /*let cellIdentifier = "cell"
+        let cellIdentifier = "cell"
         let cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)! as UITableViewCell
-        cell.textLabel?.text = String(self.scores[indexPath.row])
-        cell.detailTextLabel!.text = String(self.wow[indexPath.row])
-        return cell */
-        
-        return UITableViewCell()
+        cell.textLabel?.text = String(self.dates[indexPath.row])
+        cell.detailTextLabel!.text = String(self.dates[indexPath.row])
+        return cell
     }
     
     
