5f19c5ba75a3150743375a222174b375fa9f79d4
lucasmarzocco <lucasmarzocco@gmail.com>
2016-08-29 18:17:54 -0700

    fixing up retrieving info to post on past games tab

--- iBowl/PastScoresController.swift
+++ iBowl/PastScoresController.swift
@@ -7,16 +7,35 @@
 //
 
 import UIKit
+import Firebase
 
 class PastScoresController: UITableViewController {
     
-    var scores: [String] = ["6/7/2016", "6/10/2016", "6/14/2016", "6/27/2016"]
-    var wow: [String] = ["Average: 202 - High game: 220", "Average: 206 - High game: 300", "Average: 210 - High game: 200", "Average: 202 - High game: 189" ]
+    var ref = FIRDatabase.database().referenceFromURL("https://ibowl-c7e9e.firebaseio.com")
+    
+    
+    //var scores: [String] = ["6/7/2016", "6/10/2016", "6/14/2016", "6/27/2016"]
+    //var wow: [String] = ["Average: 202 - High game: 220", "Average: 206 - High game: 300", "Average: 210 - High game: 200", "Average: 202 - High game: 189" ]
+    
+    
+    var dates: [String] = []
+    //var keys: [String] = ["highGame", "]
     
     override func viewDidLoad() {
-    
         
-        
+        ref.observeEventType(.Value, withBlock: { snapshot in
+            
+            let postDict = snapshot.value as! [String : AnyObject]
+            
+            for item in postDict.keys {
+                print(item)
+                
+                
+                
+                
+
+            }
+        })
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
@@ -25,16 +44,18 @@ class PastScoresController: UITableViewController {
     
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return scores.count
+        return 1
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cellIdentifier = "cell"
+        /*let cellIdentifier = "cell"
         let cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)! as UITableViewCell
         cell.textLabel?.text = String(self.scores[indexPath.row])
         cell.detailTextLabel!.text = String(self.wow[indexPath.row])
-        return cell
+        return cell */
+        
+        return UITableViewCell()
     }
     
     
